c1a0e831bdd78544d4af6bde8be60ed12775965a
Sidoryx <siddhantban02@gmail.com>
2022-04-30 02:17:08 +0530

    Hero Section updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HeroSection from './Components/HeroSection';
+import HeroSection from './Components/HeroSection/HeroSection';
 
 function App()
 {
