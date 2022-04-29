5e9b0db14a58ea40b614b1b53d3a35daf4f93e9c
Sidoryx <siddhantban02@gmail.com>
2022-04-29 21:01:55 +0530

    Added the Hero section components

---
+++ frontend/src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+    <img src='image13.svg' alt='image13'/>
+    <h1>Start something Epic</h1>
+    
+    </div>
+  )
+}
+
+export default HeroSection
